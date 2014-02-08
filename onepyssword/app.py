1d1648b178b97fcedfb6dcde662e70b54f9daa73
Nikita Grishko <grin.minsk@gmail.com>
2014-02-08 18:26:21 +0300

    Add support to specify copy command

--- onepyssword/app.py
+++ onepyssword/app.py
@@ -6,16 +6,26 @@ import sys
 from difflib import get_close_matches
 from getpass import getpass
 
-from copypaste import copy
+import copypaste
 
 from onepyssword.logic import (
     get_keychain_items, get_encryption_key, unlock_encryption_key,
     load_keychain_item_data, decrypt_keychain_item_password,
 )
+from onepyssword.py3k import PY2
 
 __all__ = (
     'go',
 )
+
+
+COPY_CMD_ENV_KEY = 'ONEPYSSWORD_COPY_CMD'
+
+
+if PY2:
+    NoSuchFileOrDirectoryException = OSError
+else:
+    NoSuchFileOrDirectoryException = FileNotFoundError
 
 
 def go(argv=sys.argv[1:], stdout=sys.stdout, stderr=sys.stderr):
@@ -32,11 +42,11 @@ def go(argv=sys.argv[1:], stdout=sys.stdout, stderr=sys.stderr):
         find(keychain_items, item)
         return
 
-    password = decrypt(keychain_items, item)
+    password = decrypt(keychain_items, item, stdout=stdout, stderr=stderr)
     if arguments.prompt:
         stdout.write('{0}\n'.format(password))
     else:
-        copy(password)
+        copy(password, stdout=stdout, stderr=stderr)
         stdout.write('Password copied to clipboard!\n')
 
 
@@ -77,3 +87,25 @@ def find(keychain_items, item, stdout=sys.stdout, stderr=sys.stderr):
         stdout.write('Founded matches:\n')
         for m in matches:
             stdout.write('\t{0}\n'.format(m))
+
+
+def copy(string, stdout=sys.stdout, stderr=sys.stderr):
+    try:
+        platform = sys.platform
+        if platform not in ('darwin', 'linux', 'linux2',):
+            copypaste.copy(string)
+            return
+
+        copy_cmd = os.environ.get(COPY_CMD_ENV_KEY)
+        if not copy_cmd:
+            copypaste.copy(string)
+            return
+
+        copy_cmd = copy_cmd.split(',')
+        copypaste.copy(string, cmd=copy_cmd)
+    except NotImplementedError:
+        stderr.write('Sorry, copy command not supported for your platform.\n')
+        sys.exit(os.EX_DATAERR)
+    except NoSuchFileOrDirectoryException:
+        stderr.write('Invalid copy command or tool for copy not installed in your system.\n')
+        sys.exit(os.EX_DATAERR)
